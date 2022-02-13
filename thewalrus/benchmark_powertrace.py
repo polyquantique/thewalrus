b2daa4a33cfcd9b77a9621652e1507000b1122fd
JQZ1111 <75086618+JQZ1111@users.noreply.github.com>
2022-02-13 13:26:59 -0500

    Wrote the code to be more compact

--- thewalrus/benchmark_powertrace.py
+++ thewalrus/benchmark_powertrace.py
@@ -1,93 +1,69 @@
 import numpy as np
-import numba
+from number import jit
 import matplotlib.pyplot as plt
 from thewalrus import charpoly
-from thewalrus.charpoly import powertrace
-"""
+
+@jit(nopython = True)
 def powertrace_eigs(A,n):
-    A += A.T
-    powertrace = []  
-    A = np.linalg.matrix_power(A,n)
-    eigs = np.linalg.eigvals(A)
-    powertrace.append(np.sum(eigs))
-        
+    """
+        Get the power trace of n-1 power of A
+    """
+    powered_matrix = np.linalg.matrix_power(A,n)
+    eigs = np.linalg.eigvals(powered_matrix)
+    powertrace = np.sum(eigs)
     return powertrace
-  
-A = np.random.rand(6,6)
-A +=A.T
-
-print(powertrace_eigs(A,2))
-print(powertrace(A,3))
-
-%timeit powertrace_eigs(A,2)
-print()
-%timeit powertrace(A,3)
-
-eigs = []
-powertrace = []
-nmax = list(range(2,10,1))
-
-for a in nmax:
-    B = np.random.rand(5,5)
-    B += B.T
-    
-    
-    time_eigs = %timeit -o powertrace_eigs(B,a)
-    time_powertrace = %timeit -o powertrace(B,a)
-    eigs.append(time_eigs.average)
-    powertrace.append(time_powertrace.average)
-    
-
-plt.semilogy(nmax,eigs,nmax,powertrace)
-plt.legend(['Powertrace with eigenvalues ','Powertrace from thewalrus'])
-plt.xlabel('Matrix size')
-plt.ylabel('Time of computation')
-"""
-
-# Varying the power
-A = np.random.rand(6,6)
-A += A.T
-def powertrace_eigs(A,n):
+def powertrace_eigs_list(A, n):
     """
         Get the power trace up to n-1 power of A
     """
-    powertrace_list = []  
+    powertrace = []
     for i in range(n):
-        powered_matrix = np.linalg.matrix_power(A,i)
-        eigs = np.linalg.eigvals(powered_matrix)
-        powertrace_list.append(np.sum(eigs))        
-    return powertrace_list
-
-n = 20
-eig_powertrace = []
-charpoly_powertrace = []
-for i in range(n):
-    time_eigs = %timeit -o  powertrace_eigs(A,i)
-    time_powertrace = %timeit -o powertrace(A, i)
-    eig_powertrace.append(time_eigs.average)
-    charpoly_powertrace.append(time_powertrace.average)
-temps = np.linspace(1,20,20, dtype = int)
+        power = powertrace_eigs(A, i)
+        powertrace.append(power)
+    return powertrace
+def get_varying_power_time(A, max_power):
+    """
+        Vary the value of power
+    """
+    eig_powertrace = []
+    charpoly_powertrace = []
+    for i in range(max_power):
+        time_eigs = %timeit -o powertrace_eigs_list(A,i)
+        time_powertrace = %timeit -o charpoly.powertrace(A, i)
+        eig_powertrace.append(time_eigs.average)
+        charpoly_powertrace.append(time_powertrace.average)
+    return eig_powertrace, charpoly_powertrace
+def get_varying_size_time(max_dim, power):
+    """
+        Vary the value of power
+    """
+    eig_powertrace = []
+    charpoly_powertrace = []
+    for i in range(6,max_dim):
+        A = np.random.rand(i,i)
+        time_eigs = %timeit -o powertrace_eigs_list(A, power)
+        time_powertrace = %timeit -o charpoly.powertrace(A, power)
+        eig_powertrace.append(time_eigs.average)
+        charpoly_powertrace.append(time_powertrace.average)
+    return eig_powertrace, charpoly_powertrace
+A = np.random.rand(6,6).astype(complex)
+A += A.T
+eigs, charpol = get_varying_power_time(A, max_power = 20)
+eigs_size, charpol_size = get_varying_size_time(max_dim = 16, power = 4)
+temps1 = np.linspace(1,20,20, dtype = int)
+plot1 = plt.figure(1)
 plt.xlabel('number of powers')
 plt.ylabel('time taken on average (s)')
 plt.title("Time to find powertrace of matrix 6 by 6 for charpoly and eigenvalue algorithms")
-plt.plot(temps, eig_powertrace, label = 'eigenvalues')
-plt.plot(temps, charpoly_powertrace, label = 'charpoly')
+plt.plot(temps1, eigs, label = 'eigenvalues')
+plt.plot(temps1, charpol, label = 'charpoly')
 plt.legend(loc = 'upper right')
-# Varying the matrix size
-max_size = 16
-eig_powertrace = []
-charpoly_powertrace = []
-for i in range(6,max_size):
-    A = np.random.rand(i,i)
-    A += A.T
-    time_eigs = %timeit -o  powertrace_eigs(A,5)
-    time_powertrace = %timeit -o powertrace(A, 5)
-    eig_powertrace.append(time_eigs.average)
-    charpoly_powertrace.append(time_powertrace.average)
-temps = np.linspace(6,16,10, dtype = int)
-plt.xlabel('size of matrix')
+temps2 = np.linspace(1,10,10, dtype = int)
+plot2 = plt.figure(2)
+plt.xlabel('Size of matrix')
 plt.ylabel('time taken on average (s)')
-plt.title("Time to find powertrace for charpoly and eigenvalue algorithms for power to the 6")
-plt.plot(temps, eig_powertrace, label = 'eigenvalues')
-plt.plot(temps, charpoly_powertrace, label = 'charpoly')
+plt.title("Time to find powertrace of matrix 6 by 6 for charpoly and eigenvalue algorithms")
+plt.plot(temps2, eigs_size, label = 'eigenvalues')
+plt.plot(temps2, charpol_size, label = 'charpoly')
 plt.legend(loc = 'upper right')
+plt.show()
